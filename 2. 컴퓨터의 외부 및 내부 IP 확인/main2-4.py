16067e57a490859c72ad5d568e39423637060589
lily <choiyoonseo02@gmail.com>
2023-07-25 20:32:39 +0900

    Feat: internal&external IP checker

---
+++ 2. 컴퓨터의 외부 및 내부 IP 확인/main2-4.py
@@ -0,0 +1,15 @@
+# 컴퓨터의 외부 및 내부 IP 확인
+
+import socket
+import requests
+import re
+
+in_addr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+in_addr.connect(("www.google.com", 443))
+
+print("내부 IP: ", in_addr.getsockname()[0])
+
+req = requests.get("http://ipconfig.kr")
+out_addr = re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]
+
+print("외부 IP: ", out_addr)
